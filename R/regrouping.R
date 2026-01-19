ac5df0418a1685d96bed295e274edab7506011d4
QuentinEschmann <qeschmann@icloud.com>
2026-01-19 14:21:18 +0100

    feat: final regrouping special functions

--- R/regrouping.R
+++ R/regrouping.R
@@ -85,82 +85,34 @@ regroup_data <- function(data, dict) {
   return(data)
 }
 
-#' Combine multiple sub-question variables into one plottable multi-response variable
+
+#' renames special variables to be processed later
 #'
-#' Takes multiple variables representing sub-questions and combines each user's responses
-#' into a list, making it easy to analyze and plot individual or aggregate responses.
-#' Each response is tagged with its sub-question label for easy plotting.
+#' @param data dataset containing variables to be regrouped
+#' @param variables list of variables to be regrouped
+#' @param new_var_name base name for regrouped variables
 #'
-#' @param data A data frame containing the sub-question variables
-#' @param variables Character vector of variable names to combine
-#' @param new_var_name Name for the new combined variable
-#'
-#' @return The data frame with the new multi-response variable added (as a list column)
-#'
-#' @examples
-#' # Combine satisfaction sub-questions into one variable
-#' data <- regroup_special_var(data, c("v_1", "v_2", "v_3"), "satisfaction_combined")
-#' # Access user 1's responses: data$satisfaction_combined[[1]]
-regroup_special_var <- function(data, variables, new_var_name, new_var_label) {
-  # Get the first variable's labels to use as levels
-  first_var_labels <- attr(data[[variables[1]]], "labels")
-
-  # Get sub-question labels
-  subquestion_labels <- sapply(variables, function(var) {
-    label <- attr(data[[var]], "label")
-    if (is.null(label)) return(var)
-    return(label)
-  })
-
-  # For each row (user), collect their responses to all sub-questions
-  user_responses <- vector("list", nrow(data))
-
-  for (i in seq_len(nrow(data))) {
-    responses <- rep(NA_character_, length(variables)) # Initialize with NAs
-
-    for (j in seq_along(variables)) {
-      var <- variables[j]
-      var_value <- data[[var]][i]
-
-      # Skip if NULL, length 0, or NA
-      if (is.null(var_value) || length(var_value) == 0 || is.na(var_value)) next
-
-      # Get the value labels (if available)
-      value_labels <- attr(data[[var]], "labels")
-
-      if (!is.null(value_labels)) {
-        # Find the label for this value
-        response_label <- names(value_labels)[value_labels == var_value]
-        if (length(response_label) > 0) {
-          responses[j] <- response_label[1]
-        } else {
-          responses[j] <- as.character(var_value)
-        }
-      } else {
-        responses[j] <- as.character(var_value)
-      }
+#' @return modified dataset
+regroup_special_var <- function(data, variables, new_var_name) {
+  cnt <- 1
+  for (var in variables) {
+    if (!var %in% names(data)) {
+      stop(paste0("Variable '", var, "' not found in data"))
     }
 
-    # Store this user's responses as factor with names attribute for sub-questions
-    if (!is.null(first_var_labels)) {
-      level_names <- names(first_var_labels)
-      user_responses[[i]] <- factor(responses, levels = level_names)
-    } else {
-      user_responses[[i]] <- factor(responses)
-    }
+    new_name <- paste0(new_var_name, "_", cnt)
 
-    # Add sub-question labels as names
-    names(user_responses[[i]]) <- subquestion_labels
+    # Copy the variable with all its attributes
+    data[[new_name]] <- data[[var]]
+
+    cnt <- cnt + 1
+
+    # Remove the old variable
+    data[[var]] <- NULL
   }
-
-  # Add the new list column
-  data[[new_var_name]] <- user_responses
-  attr(data[[new_var_name]], "label") <- new_var_label
-  attr(data[[new_var_name]], "labels") <- subquestion_labels
 
   return(data)
 }
-
 
 #' Apply multi-response combination to multiple question groups
 #'
@@ -170,7 +122,8 @@ regroup_special_var <- function(data, variables, new_var_name, new_var_label) {
 #' @param new_var_labels Character vector of labels for each new variable
 #'
 #' @return The data frame with all combined variables added
-regroup_special <- function(data, variables_list, starting_new_var_num, new_var_labels) {
+regroup_special <- function(data, variables_list) {
+  starting_new_var_num <- 700
   cnt <- 0
   for (vars in variables_list) {
     new_var_name <- paste0("v_", starting_new_var_num + cnt)
@@ -193,7 +146,7 @@ regroup_special <- function(data, variables_list, starting_new_var_num, new_var_
 
     # Only process if there are variables left after removing "Autre"
     if (length(vars_to_keep) > 0) {
-      data <- regroup_special_var(data, vars_to_keep, new_var_name, new_var_labels[cnt + 1])
+      data <- regroup_special_var(data, vars_to_keep, new_var_name)
     } else {
       warning(paste0("No variables to combine for ", new_var_name, " after removing 'Autre' variables"))
     }
